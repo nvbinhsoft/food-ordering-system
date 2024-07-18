aa0dc0a3ac0c21e5bd7e6ea4f7d31829568fa0d1
Binh Nguyen Van <nvbinhsoft@gmail.com>
2024-07-19 00:32:31 +0700

    fix: fix kafka helper

--- infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/KafkaMessageHelper.java
+++ infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/KafkaMessageHelper.java
@@ -2,8 +2,7 @@ package com.food.ordering.system.kafka.producer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
-import com.food.ordering.system.outbox.OutboxStatus;
+import com.food.ordering.tech.order.service.domain.exception.OrderDomainException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.kafka.support.SendResult;
